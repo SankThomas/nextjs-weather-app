c11aee9c677653611491d39d76e8ee69a0d11162
Thomas Sankara <thomassankara74@gmail.com>
2021-04-04 13:33:00 +0300

    fetch data

--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,47 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React from "react"
+import Head from "next/head"
+import styles from "../styles/Home.module.css"
 
-export default function Home() {
+export default function Home({ cities }) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>GitHub Users NextJs App</title>
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.4/tailwind.min.css"
+        />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+      <section className="px-5 mb-10 bg-white py-10 shadow-lg mx-5 rounded-sm">
+        <h3 className="font-bold text-center text-xl mb-10">
+          You are currently viewing Weather information for {cities.name}
+        </h3>
+        <h1 className="font-bold text-4xl mb-5">
+          {cities.name}, {cities.sys.country}
         </h1>
+        <ul>
+          <h4 className="font-bold">Geographical Location</h4>
+          <li>Latitude: {cities.coord.lat}</li>
+          <li>Longitude: {cities.coord.lon}</li>
+        </ul>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className="mt-5">
+          {cities.weather.map(({ id, main, description }) => {
+            return (
+              <div key={id}>
+                <h4 className="font-bold">Current Weather: {main}</h4>
+                <p className="capitalize">More information: {description}</p>
+              </div>
+            )
+          })}
         </div>
-      </main>
+
+        <small className="block mt-5">
+          Weather information updates automatically from the API.
+        </small>
+      </section>
 
       <footer className={styles.footer}>
         <a
@@ -56,10 +49,23 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=nairobi&appid=${process.env.REACT_APP_API_KEY}`
+  )
+  const cities = await res.json()
+
+  return {
+    props: {
+      cities,
+    },
+  }
 }
